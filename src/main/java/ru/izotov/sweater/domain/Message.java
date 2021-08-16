6fb15aa91828793d66ae8914d5e69ad4687b85bc
Сергей <starback1990@gmail.com>
2021-08-16 23:53:03 +0700

    lesson 7. Загрузка файлов на сервер и раздача статики.

--- src/main/java/ru/izotov/sweater/domain/Message.java
+++ src/main/java/ru/izotov/sweater/domain/Message.java
@@ -16,6 +16,10 @@ public class Message {
 
     private User author;
 
+    private String filename;
+
+
+
     public Message() {
     }
 
@@ -54,6 +58,14 @@ public class Message {
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 
     @Override
