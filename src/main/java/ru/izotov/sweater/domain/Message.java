541396456dbf914d373ef9afbb26de3fc55ddbfa
Сергей <starback1990@gmail.com>
2021-10-08 21:19:05 +0700

    Lesson 10. Spring Boot FlyWay: migration DB, profile.

--- src/main/java/ru/izotov/sweater/domain/Message.java
+++ src/main/java/ru/izotov/sweater/domain/Message.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
     private String tag;
@@ -32,8 +32,13 @@ public class Message {
     public String getText() {
         return text;
     }
-    public Integer getId() {
+
+    public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setText(String text) {
