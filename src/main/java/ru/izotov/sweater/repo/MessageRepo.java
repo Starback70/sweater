d6e8c4e5ff26211138de3408752269af90ba9e48
Сергей <starback1990@gmail.com>
2021-11-02 21:26:50 +0700

    Part 19. Spring Boot: adding likes, use HQL (JPQL).

--- src/main/java/ru/izotov/sweater/repo/MessageRepo.java
+++ src/main/java/ru/izotov/sweater/repo/MessageRepo.java
@@ -1,11 +1,41 @@
 package ru.izotov.sweater.repo;
 
-import ru.izotov.sweater.domain.Message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import ru.izotov.sweater.domain.Message;
+import ru.izotov.sweater.domain.User;
+import ru.izotov.sweater.domain.dto.MessageDto;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
-    Page<Message> findAll(Pageable pageable);
-    Page<Message> findByTag(String tag, Pageable pageable);
+
+    @Query("select  new ru.izotov.sweater.domain.dto.MessageDto(" +
+            "m, " +
+            "count(ml), " +
+            "sum(case when ml = :user then  1 else 0 end) > 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "group by m ")
+    Page<MessageDto> findAll(Pageable pageable, @Param("user") User user);
+    @Query("select  new ru.izotov.sweater.domain.dto.MessageDto(" +
+            "m, " +
+            "count(ml), " +
+            "sum(case when ml = :user then  1 else 0 end) > 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "where m.tag = :tag " +
+            "group by m ")
+    Page<MessageDto> findByTag(@Param("tag") String tag, Pageable pageable, @Param("user") User user);
+
+    @Query("select  new ru.izotov.sweater.domain.dto.MessageDto(" +
+            "m, " +
+            "count(ml), " +
+            "sum(case when ml = :user then  1 else 0 end) > 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "where m.author = :author " +
+            "group by m ")
+    Page<MessageDto> findByUser(Pageable pageable, @Param("author") User author, @Param("user") User user);
 }
